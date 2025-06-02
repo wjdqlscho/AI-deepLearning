9e36afaec2132b3abf4abb66db179144287d9e91
정빈 <169121343+wjdqlscho@users.noreply.github.com>
2025-06-02 14:32:09 +0900

    Update class_inheritance.py

--- class_inheritance.py
+++ class_inheritance.py
@@ -17,17 +17,14 @@ class Student:
     def add_age(self, offset):
         self.age += offset
 
-# 학생 객체 생성
 student1 = Student("200000", "홍길동", 20, "남성", "컴퓨터공학과")
 student2 = Student("232100", "조정빈", 25, "남성", "소프트웨어학과")
 student3 = Student("200532", "표창원", 60, "남성", "교통에너지융합학과")
 
-# 학생 정보 출력
 student1.show()
 student2.show()
 student3.show()
 
-# 새로운 클래스 jungbin 정의
 class Jungbin(Student):
     def __init__(self, id, name, age, gender, department, subject):
         super().__init__(id, name, age, gender, department)
@@ -39,6 +36,5 @@ class Jungbin(Student):
         print(f"jungbin's age : {self.age}")
         print(f"jungbin's subject : {self.subject}")
 
-# jungbin 객체 생성 및 출력
 jojungbin = Jungbin("232100", "조정빈", 25, "남성", "소프트웨어학과", "소프트웨어")
 jojungbin.show_jungbin()
